fab7aa6d3ae068e7bf7ebb8a700e34b205f6aaa2
Victor Sandru <vsandru4@gmail.com>
2022-04-15 00:07:40 +0200

    added login for OpenSky

--- python/main.py
+++ python/main.py
@@ -1,6 +1,15 @@
+import requests
+import json
 from opensky_api import OpenSkyApi
 
 api = OpenSkyApi()
-s = api.get_states()
+
+#log into OpenSkyApi
+data = json.load(open('items.json'))
+api.__init__(data['user'],data['pass'])
+
+planeHex = "4d229d"
+
+s = api.get_states(0,planeHex)
 
 print(s)
