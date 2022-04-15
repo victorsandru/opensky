12c9ca3bec70bd18d72fdfbc18d82b0e5e79f1ba
Victor Sandru <vsandru4@gmail.com>
2022-04-15 14:00:41 +0200

    updated main

--- python/main.py
+++ python/main.py
@@ -8,8 +8,12 @@ api = OpenSkyApi()
 data = json.load(open('items.json'))
 api.__init__(data['user'],data['pass'])
 
-planeHex = "4d229d"
+planeHex = "3c4b23"
 
 s = api.get_states(0,planeHex)
+states = s.__dict__['states'][0].__dict__
 
-print(s)
+
+# print(s)
+print(states['origin_country'])
+print("long: " + str(states['longitude']) + "\nlat: " + str(states['latitude']))
